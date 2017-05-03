10cb30dcd10c551b734cfb94cb1a63517aec9622
lbisson <lbisson@gmail.com>
2017-05-03 19:38:27 -0400

    email cap ready for demo

--- src/app/solicitation.service.ts
+++ src/app/solicitation.service.ts
@@ -14,15 +14,15 @@ export class SolicitationService {
   pushedSolicitation = new EventEmitter();
 
   constructor ( private http: Http ){};
-  // private solicitationsUrl = 'http://ec2-54-145-198-134.compute-1.amazonaws.com:3000/predictions';
-  // private solicitationsFilterUrl = 'http://ec2-54-145-198-134.compute-1.amazonaws.com:3000/predictions/filter';
-  // private solicitationUrl = 'http://ec2-54-145-198-134.compute-1.amazonaws.com:3000/solicitation/';
-  // emailUrl = 'http://ec2-54-145-198-134.compute-1.amazonaws.com:3000/email/';
+  private solicitationsUrl = 'http://ec2-54-145-198-134.compute-1.amazonaws.com:3000/predictions';
+  private solicitationsFilterUrl = 'http://ec2-54-145-198-134.compute-1.amazonaws.com:3000/predictions/filter';
+  private solicitationUrl = 'http://ec2-54-145-198-134.compute-1.amazonaws.com:3000/solicitation/';
+  emailUrl = 'http://ec2-54-145-198-134.compute-1.amazonaws.com:3000/email/';
 
-   private solicitationsUrl = 'http://localhost:3000/predictions';
-   private solicitationsFilterUrl = 'http://localhost:3000/predictions/filter';
-   private solicitationUrl = 'http://localhost:3000/solicitation/';
-   private emailUrl = 'http://localhost:3000/email/';
+  //  private solicitationsUrl = 'http://localhost:3000/predictions';
+  //  private solicitationsFilterUrl = 'http://localhost:3000/predictions/filter';
+  //  private solicitationUrl = 'http://localhost:3000/solicitation/';
+  //  private emailUrl = 'http://localhost:3000/email/';
 
   getFilteredSolicitations(body) {
 		console.log(body);
@@ -50,7 +50,7 @@ export class SolicitationService {
       .catch((error:any) => Observable.throw(error.json().error || 'Server Error'));
     }
 
-  
+
     getData() {
         // Get whole whole data set
         var data =  this.http.get(this.solicitationsUrl).map((res: Response) => res.json());
