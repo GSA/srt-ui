0bc7df706784447607a326cda9d398e4d52d4647
Yung-Sheng Chen <ysscott2012@gmail.com>
2017-05-30 16:03:45 -0400

    chart2

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild, Directive } from '@angular/core';
 import { Router } from '@angular/router';
 import { SolicitationService } from '../solicitation.service';
-import { BaseChartDirective } from 'ng2-charts';
-import {ChartsModule, Color} from 'ng2-charts';
+import { BaseChartDirective } from 'ng2-charts/ng2-charts';
+import {ChartsModule, Color} from 'ng2-charts/ng2-charts';
 
 @Component({
   selector: 'app-analytics',
