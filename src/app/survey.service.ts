6d1118721b1347593e1a13f9d5fd8c5b8a886d1a
Yung-Sheng Chen <ysscott2012@gmail.com>
2017-07-27 23:23:33 -0400

    srt

--- src/app/survey.service.ts
+++ src/app/survey.service.ts
@@ -9,8 +9,8 @@ import { Survey } from './shared/survey';
 export class SurveyService {
 
   // productionURL
-  private link = 'http://ec2-54-145-198-134.compute-1.amazonaws.com:3000';
-  // private link = 'http://localhost:3000';
+  // private link = 'http://ec2-54-145-198-134.compute-1.amazonaws.com:3000';
+  private link = 'http://localhost:3000';
 
   private surveysURL = this.link + '/surveys';
 
