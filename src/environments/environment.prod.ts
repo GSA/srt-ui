3b91fc9654dda3be4659eb62cdef56b4eaf58473
Al Crowley <albert.crowley@tcg.com>
2019-01-15 19:21:37 +0000

    cloud.gov settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  SERVER_URL: 'http://localhost:3000',
+  SERVER_URL: 'https://srt-server.app.cloud.gov',
   EMAIL_NOTIFICATION: true,
-  FILE_UPLOAD_API: 'http://localhost:3000/file/upload'
+  FILE_UPLOAD_API: 'http://srt-server.app.cloud.gov/file/upload'
 
 };
