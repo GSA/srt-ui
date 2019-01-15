800e084d750da3dd6da2e26eee2a5d3c0b62772a
Al Crowley <albert.crowley@tcg.com>
2019-01-15 14:38:46 -0500

    Merged

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,4 +4,8 @@ export const environment = {
   EMAIL_NOTIFICATION: true,
   FILE_UPLOAD_API: 'http://srt-server.app.cloud.gov/file/upload'
 
+  // SERVER_URL: 'http://localhost:3000',
+  // EMAIL_NOTIFICATION: true,
+  // FILE_UPLOAD_API: 'http://localhost:3000/file/upload'
+
 };
