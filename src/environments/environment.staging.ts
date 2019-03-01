52deaa8b66ed1872e2869ffcdbec34d342eb6f72
Al Crowley <albert.crowley@tcg.com>
2019-03-01 14:08:05 -0500

    Adding staging config

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,11 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: true,
+  SERVER_URL: 'https://srt-server-staging.app.cloud.gov/api',
+  EMAIL_NOTIFICATION: true,
+  FILE_UPLOAD_API: 'http://srt-server-staging.app.cloud.gov/file/upload'
+};
