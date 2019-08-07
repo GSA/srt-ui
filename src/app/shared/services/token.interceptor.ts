49fb30fadd33a7894aa94338783664560a7c411b
Al Crowely <albert.crowley@tcg.com>
2019-08-06 23:20:31 -0400

    Show the login page if the user session times out.

--- src/app/shared/services/token.interceptor.ts
+++ src/app/shared/services/token.interceptor.ts
@@ -8,11 +8,13 @@ import {
 import { Observable } from 'rxjs/Observable';
 import { environment } from '../../../environments/environment';
 import { TokenService } from './token.service';
+import {Router} from '@angular/router';
 
 const state = {
   token_expires : 0,
-  refresh_queued : false
-}
+  refresh_queued : false,
+  session_timer: null
+};
 
 /**
  * Makes a request to renew the current JWT
@@ -20,19 +22,35 @@ const state = {
  * @param request
  * @param http
  * @param TokenService
+ * @param router
  */
-function refresh (request: HttpRequest<any>, http: HttpClient, TokenService: TokenService) {
-    http.get(environment.SERVER_URL + '/renewToken')
-      .subscribe((data: any) => {
-        localStorage.setItem('token', data.token);
-        state.refresh_queued = false;
-      });
+function refresh (request: HttpRequest<any>, http: HttpClient, TokenService: TokenService, router: Router) {
+  http.get(environment.SERVER_URL + '/renewToken')
+    .subscribe((data: any) => {
+      localStorage.setItem('token', data.token);  // got the new token, so save it
+      state.refresh_queued = false;  // clear out the queue marker
+      const session_timeout =  data.token_life_in_seconds * 1000 || 30 * 60 * 1000;  // default to 30 minutes
+
+      // if there is a session timeout session_timer pending, clear it.
+      if (state.session_timer) {
+        window.clearTimeout(state.session_timer);
+      }
+
+      // create a new session_timer to send the user to the login page if they timeout
+      state.session_timer = window.setTimeout(() => {
+        console.log('session timeout');
+        return router.navigate(['/auth']);
+      }, session_timeout);
+    }, (error) => {
+      console.log('error renewing token', error);
+    });
 }
+
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor(public http: HttpClient, public tokenService: TokenService) {}
+  constructor(public http: HttpClient, public tokenService: TokenService, private router: Router ) {}
 
   /**
    * This interceptor is run for every HTTPS call.
@@ -47,7 +65,7 @@ export class TokenInterceptor implements HttpInterceptor {
 
     // we don't want to flood the server. Max request rate is once every 5 seconds.
     if ( (! state.refresh_queued) && (!request.url.match('version')) ) {
-      setTimeout(() => refresh(request, this.http, this.tokenService), 5000);
+      setTimeout(() => refresh(request, this.http, this.tokenService, this.router), 5000);
       state.refresh_queued = true;
     }
 
@@ -57,7 +75,16 @@ export class TokenInterceptor implements HttpInterceptor {
         Authorization: `Bearer ${token}`
       }
     });
-    return next.handle(request);
+    return next.handle(request).do(
+      () => { },
+      (error: any) => {
+        console.log(error);
+        if (error.status === 401) {
+          console.log ('navigating away');
+          return this.router.navigate(['/auth']);
+        }
+      }
+    );
   }
 }
 
