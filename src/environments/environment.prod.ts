eaa1106404102fa7ff04d6123c3fafeec3b1b321
Al Crowely <albert.crowley@tcg.com>
2019-09-26 12:41:30 -0400

    Removing some optomizations from production builds.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   SERVER_URL: 'https://srt-server.app.cloud.gov/api',
   EMAIL_NOTIFICATION: true,
   FILE_UPLOAD_API: 'http://srt-server.app.cloud.gov/file/upload'
