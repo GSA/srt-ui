278d79c6afb8e4b35277caef78d8580292d0de79
Adam Buckingham <AJBuckingham7@gmail.com>
2024-05-21 10:01:39 -0400

    Fixing angular package building with zone.js 0.14.0. See https://stackoverflow.com/questions/77469988/error-module-not-found-error-package-path-dist-zone-is-not-exported-from-pa

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -62,8 +62,9 @@ import 'core-js/es/reflect';
  * Zone JS is required by Angular itself.
  */
 (window as any).__Zone_enable_cross_context_check = true;
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+// Included with Angular CLI.
+import 'zone.js';
+import 'zone.js/testing';
 
 
 /***************************************************************************************************
