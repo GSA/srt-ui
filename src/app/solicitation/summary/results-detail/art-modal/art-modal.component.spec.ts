52f40ad72d15afb629b1ddea6dbbe9d042a98906
Adam Buckingham <AJBuckingham7@gmail.com>
2024-11-18 09:59:04 -0500

    Fixed art-modal component test

--- src/app/solicitation/summary/results-detail/art-modal/art-modal.component.spec.ts
+++ src/app/solicitation/summary/results-detail/art-modal/art-modal.component.spec.ts
@@ -1,6 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ArtIframeDialogComponent } from './art-modal.component';
+import { ArtService } from './art.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SolicitationService } from '../../../solicitation.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { CheckboxModule } from 'primeng/checkbox';
+
+import { ArtCategoryComponent } from './art-category/art-category.component';
 
 describe('ArtIframeDialogComponent', () => {
   let component: ArtIframeDialogComponent;
@@ -8,7 +19,11 @@ describe('ArtIframeDialogComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ArtIframeDialogComponent ]
+      declarations: [ ArtIframeDialogComponent, ArtCategoryComponent ],
+      imports: [HttpClientTestingModule, RouterTestingModule.withRoutes([]),
+            CommonModule, FormsModule, ButtonModule, DialogModule, 
+            CheckboxModule],
+      providers: [ArtService, SolicitationService]
     })
     .compileComponents();
 
