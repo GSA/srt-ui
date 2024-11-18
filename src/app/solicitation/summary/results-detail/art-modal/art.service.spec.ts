a4093efcc58aed88c372c080e336580ee63c3917
Adam Buckingham <AJBuckingham7@gmail.com>
2024-11-18 09:49:45 -0500

    Fix Art Service test

--- src/app/solicitation/summary/results-detail/art-modal/art.service.spec.ts
+++ src/app/solicitation/summary/results-detail/art-modal/art.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ArtService } from './art.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ArtService', () => {
   let service: ArtService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      declarations: [],
+      imports: [HttpClientTestingModule],
+      providers: [ArtService]
+    });
     service = TestBed.inject(ArtService);
   });
 
