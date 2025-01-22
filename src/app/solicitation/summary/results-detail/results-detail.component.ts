b42cb5441ab7c2219c9dbb32567dfe0e9c0f2bb1
Collin Schreyer <collin.schreyer@gsa.gov>
2025-01-22 11:14:37 -0600

    Commit to address all comments from previous and current PRs

--- src/app/solicitation/summary/results-detail/results-detail.component.ts
+++ src/app/solicitation/summary/results-detail/results-detail.component.ts
@@ -1,170 +1,156 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
-
 import { SolicitationService } from '../../solicitation.service';
 import { Solicitation } from '../../../shared/solicitation';
 import * as moment from 'moment';
-import {environment} from 'environments/environment';
-
+import { environment } from 'environments/environment';
 import { GoogleAnalyticsService } from 'ngx-google-analytics';
+
+interface ParseStatus {
+  formattedDate: string;
+  postedDate: Date | null;
+  name: string;
+  status: string;
+  attachment_url: string;
+}
 
 @Component({
   selector: 'app-results-detail',
   templateUrl: './results-detail.component.html',
   styleUrls: ['./results-detail.component.scss']
 })
-
-
-
 export class ResultsDetailComponent implements OnInit {
-
-  /* ATTRIBUTES */
-
-  public lockDocs;
-
+  public lockDocs: number[];
   solicitation: Solicitation;
   subscription: Subscription;
-  solicitationID: String;
-  type: String = 'report';
-  public step1: Boolean = false;
-  public step2: Boolean = false;
-  public step3: Boolean = false;
+  solicitationID: string;
+  type: string = 'report';
+  public step1: boolean = false;
+  public step2: boolean = false;
+  public step3: boolean = false;
   feature_flags = environment.feature_flags;
 
+  private readonly STEP_ACTIONS = {
+    REVIEW: 'reviewed solicitation action requested summary',
+    EMAIL: 'sent email to POC',
+    FEEDBACK: 'provided feedback on the solicitation prediction result'
+  };
 
-  /* CONSTRUCTORS */
-
-  /**
-   * constructor
-   * @param solicitationService
-   * @param router
-   * @param route
-   */
   constructor(
     private solicitationService: SolicitationService,
     private router: Router,
     private route: ActivatedRoute,
-    private $gaService: GoogleAnalyticsService
+    private gaService: GoogleAnalyticsService
   ) {
     this.solicitation = new Solicitation(null, null, null, null, null, null,
-      {value: ''}, null, null, null, null, null, null,
+      { value: '' }, null, null, null, null, null, null,
       [{ name: '', status: '', attachment_url: '', formattedDate: '', postedDate: new Date() }],
-      [''], null, null, {psc: '', naics: '', naics_match: false, epa_psc_match: false},
+      [''], null, null, { psc: '', naics: '', naics_match: false, epa_psc_match: false },
       null, null, null, null, true);
     this.solicitation.na_flag = false;
   }
 
-  /**
-   * lifecycle
-   */
   ngOnInit() {
-    const navigation = this.router.getCurrentNavigation();
-    if (navigation?.extras?.state?.['solicitation']) {
-      const data = navigation.extras.state['solicitation'];
-      
-      if (data.parseStatus && Array.isArray(data.parseStatus)) {
-        data.parseStatus.forEach(element => {
-          element.status = element.status === 'successfully parsed' ? 'Yes' : 
-                          element.status === 'processing error' ? 'No' : 
-                          element.status;
-          element.formattedDate = moment(element.postedDate).format('L');
-        });
+    try {
+      const navigation = this.router.getCurrentNavigation();
+      if (navigation?.extras?.state?.['solicitation']) {
+        const data = navigation.extras.state['solicitation'];
+        this.processSolicitationData(data);
       } else {
-        console.log('Error processing parse status for solicitation ' + data.solNum);
-        data.parseStatus = [{
-          formattedDate: '', 
-          postedDate: null, 
-          name: '', 
-          status: '', 
-          attachment_url: ''
-        }];
+        this.loadSolicitationData();
       }
-   
-      this.step1 = data.history?.filter(e => 
-        e['action'].indexOf('reviewed solicitation action requested summary') > -1
-      ).length > 0;
-   
-      this.step2 = data.history?.filter(e =>
-        e['action'].indexOf('sent email to POC') > -1
-      ).length > 0;
-   
-      this.step3 = data.history?.filter(e =>
-        e['action'].indexOf('provided feedback on the solicitation prediction result') > -1
-      ).length > 0;
-   
-      this.solicitation = data;
-      this.solicitationID = data.id;
-   
-      const totalDoc = Number(this.solicitation.numDocs);
-      if (!isNaN(totalDoc)) {
-        if (totalDoc !== this.solicitation.parseStatus.length) {
-          const lock = totalDoc - this.solicitation.parseStatus.length;
-          this.lockDocs = Array(lock).fill(0).map((_, i) => i + 1);
-        }
-      }
-   
-    } else {
-      this.subscription = this.route.params.subscribe(params => {
-        this.solicitationID = params['id'];
-        this.solicitationService.getSolicitation(this.solicitationID).subscribe({
-          next: data => {
-            if (data.parseStatus && Array.isArray(data.parseStatus)) {
-              data.parseStatus.forEach(element => {
-                element.status = element.status === 'successfully parsed' ? 'Yes' : 
-                                element.status === 'processing error' ? 'No' : 
-                                element.status;
-                element.formattedDate = moment(element.postedDate).format('L');
-              });
-            } else {
-              console.log('Error processing parse status for solicitation ' + data.solNum);
-              data.parseStatus = [{
-                formattedDate: '', 
-                postedDate: null,
-                name: '',
-                status: '',
-                attachment_url: ''
-              }];
-            }
-   
-            this.step1 = data.history?.filter(e =>
-              e['action'].indexOf('reviewed solicitation action requested summary') > -1
-            ).length > 0;
-   
-            this.step2 = data.history?.filter(e =>
-              e['action'].indexOf('sent email to POC') > -1
-            ).length > 0;
-   
-            this.step3 = data.history?.filter(e =>
-              e['action'].indexOf('provided feedback on the solicitation prediction result') > -1
-            ).length > 0;
-   
-            this.solicitation = data;
-   
-            const totalDoc = Number(this.solicitation.numDocs);
-            if (!isNaN(totalDoc)) {
-              if (totalDoc !== this.solicitation.parseStatus.length) {
-                const lock = totalDoc - this.solicitation.parseStatus.length;
-                this.lockDocs = Array(lock).fill(0).map((_, i) => i + 1);
-              }
-            }
-          },
-          error: err => console.log(err)
-        });
-      });
+    } catch (error) {
+      console.error('Error initializing component:', error);
+      // Handle error appropriately - could add error state to component
     }
-   }
-   
-  onNotApplicableClick(event) {
+  }
+
+  private loadSolicitationData(): void {
+    this.subscription = this.route.params.subscribe(params => {
+      this.solicitationID = params['id'];
+      this.solicitationService.getSolicitation(this.solicitationID).subscribe({
+        next: data => this.processSolicitationData(data),
+        error: err => console.error('Error fetching solicitation:', err)
+      });
+    });
+  }
+
+  private processSolicitationData(data: any): void {
+    try {
+      this.processParseStatus(data);
+      this.processSteps(data);
+      this.setSolicitationData(data);
+      this.processDocuments();
+    } catch (error) {
+      console.error('Error processing solicitation data:', error);
+      // Handle error appropriately
+    }
+  }
+
+  private processParseStatus(data: any): void {
+    if (data.parseStatus && Array.isArray(data.parseStatus)) {
+      data.parseStatus.forEach(element => {
+        element.status = this.mapStatus(element.status);
+        element.formattedDate = moment(element.postedDate).format('L');
+      });
+    } else {
+      console.error('Error processing parse status for solicitation ' + data.solNum);
+      data.parseStatus = this.getDefaultParseStatus();
+    }
+  }
+
+  private mapStatus(status: string): string {
+    return status === 'successfully parsed' ? 'Yes' :
+           status === 'processing error' ? 'No' :
+           status;
+  }
+
+  private getDefaultParseStatus(): ParseStatus {
+    return {
+      formattedDate: '',
+      postedDate: null,
+      name: '',
+      status: '',
+      attachment_url: ''
+    };
+  }
+
+  private processSteps(data: any): void {
+    this.step1 = this.checkStepCompletion(data.history, this.STEP_ACTIONS.REVIEW);
+    this.step2 = this.checkStepCompletion(data.history, this.STEP_ACTIONS.EMAIL);
+    this.step3 = this.checkStepCompletion(data.history, this.STEP_ACTIONS.FEEDBACK);
+  }
+
+  private checkStepCompletion(history: any[], actionText: string): boolean {
+    return history?.filter(e => e['action'].indexOf(actionText) > -1).length > 0;
+  }
+
+  private setSolicitationData(data: any): void {
+    this.solicitation = data;
+    this.solicitationID = data.id;
+  }
+
+  private processDocuments(): void {
+    const totalDoc = Number(this.solicitation.numDocs);
+    if (!isNaN(totalDoc) && totalDoc !== this.solicitation.parseStatus.length) {
+      const lock = totalDoc - this.solicitation.parseStatus.length;
+      this.lockDocs = Array(lock).fill(0).map((_, i) => i + 1);
+    }
+  }
+
+  onNotApplicableClick(event: any): void {
     this.solicitation.na_flag = event.target.checked;
     this.solicitationService.update(this.solicitation)
-      .subscribe( (data) => {});
+      .subscribe({
+        next: () => {},
+        error: (err) => console.error('Error updating solicitation:', err)
+      });
 
-      this.$gaService.event('not_applicable', 'make_srt_better', 'Not Applicable');
-
+    this.gaService.event('not_applicable', 'make_srt_better', 'Not Applicable');
   }
 
   onClickTabs(action: string, label: string): void {
-    this.$gaService.event(action, "solicitation_tab", label);
+    this.gaService.event(action, "solicitation_tab", label);
   }
 }
